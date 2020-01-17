6882f348fd5a9baf1ee5960a56175b48135756b7
Tolga Erkilet <Tolga.Erkilet@Realcore.de>
2020-01-17 12:43:11 +0100

    Default Scaffold

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HBS_Test_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
